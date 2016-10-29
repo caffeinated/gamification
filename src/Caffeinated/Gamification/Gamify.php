23808a0d80e7ec5af96fec5b3f6db7bf2efbb6bc
Xander Smalbil <xander@videofunk.nl>
2016-10-29 20:47:45 +0200

    Added some code to get things started

--- src/Caffeinated/Gamification/Gamify.php
+++ src/Caffeinated/Gamification/Gamify.php
@@ -1,7 +1,27 @@
-<?php
-namespace Caffeinated\Gamification;
+<?php namespace Caffeinated\Gamification;
+
+use Models\Badge;
 
 class Gamify
 {
-	// 
+
+	public function give($points = 0, $badge = null) 
+    {
+        if(!is_null($badge)) return $this->givePointsToBadge($points, $badge);
+        return $this->givePointsToUser($points);
+    }
+
+    private function givePointsToUser($points)
+    {
+        Auth::user()->points += $points;
+        return Auth::user()->save();
+    }
+
+    private function givePointsToBadge($points, $badge)
+    {
+        if(Badge::byName($badge) && Auth::user()->hasBadge($badge)) {
+            Auth::user()->badge->where('name', $badge)->points += $points;
+            return Auth::user()->badge->save();
+        }
+    }
 }
